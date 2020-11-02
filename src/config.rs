7b061cdca7829a37e2877da50fa24eb1de7ae996
石博文 <sbw@sbw.so>
2020-11-02 22:04:26 +0800

    set default for config fields

--- src/config.rs
+++ src/config.rs
@@ -3,10 +3,13 @@ use std::collections::HashMap;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Config {
+    #[serde(default)]
     pub auth: bool,
     pub listen_addr: String,
     pub private_cert: Option<String>,
     pub cert_chain: Option<String>,
+    #[serde(default)]
     pub job_limits: HashMap<String, u64>,
+    #[serde(default)]
     pub allow_tokens: Vec<String>,
 }
